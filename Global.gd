fd5a3bb71a84dd4c51e090c6c59211615463f7f2
lucas sckenal <lucaspsckenal@gmail.com>
2026-03-09 16:14:30 -0300

    Add main menu and character selection functionality
    - Created Global.gd to store character paths and a list of available characters.
    - Implemented main_menu.gd with button functionalities for starting the game, customizing the character, and accessing settings.
    - Designed main_menu.tscn scene with buttons for user interaction.

---
+++ Global.gd
@@ -0,0 +1,20 @@
+extends Node
+
+# Aqui vamos guardar o caminho (cena) do personagem escolhido
+var personagem_escolhido_path : String = ""
+
+# Você pode colocar as cenas dos seus personagens aqui depois
+var lista_personagens = [
+	"res://Personagens/character-female-a.glb",
+	"res://Personagens/character-female-b.glb",
+	"res://Personagens/character-female-c.glb",
+	"res://Personagens/character-female-d.glb",
+	"res://Personagens/character-female-e.glb",
+	"res://Personagens/character-female-f.glb",
+	"res://Personagens/character-male-a.glb",
+	"res://Personagens/character-male-b.glb",
+	"res://Personagens/character-male-c.glb",
+	"res://Personagens/character-male-d.glb",
+	"res://Personagens/character-male-e.glb",
+	"res://Personagens/character-male-f.glb"
+]
