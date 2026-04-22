90512c0cd9d7830bda5f857d8fb70aa2ce14893e
LuanVitorCD <luanvitorcd@gmail.com>
2026-04-21 22:48:32 -0300

    feat: music now loops

--- Scripts Locais/musica_global.gd
+++ Scripts Locais/musica_global.gd
@@ -6,6 +6,14 @@ var musica_deserto = preload("res://Musicas/Oasis_at_Noon.mp3")
 var musica_covil = preload("res://Musicas/The_Serpent_s_Last_March.mp3")
 var musica_bruxa = preload("res://Musicas/Crooked_Path_to_the_Bayou.mp3")
 
+# Definindo propriedade para ter loop na música (agradecemos Bira pelo beta test ao vivo)
+func _ready():
+	musica_menu.loop = true
+	musica_tutorial.loop = true
+	musica_deserto.loop = true
+	musica_covil.loop = true
+	musica_bruxa.loop = true
+
 # Função chamada pelos menus para garantir que o tema principal esteja tocando
 func tocar_menu():
 	if stream != musica_menu:
