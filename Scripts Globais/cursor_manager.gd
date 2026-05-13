9ad2d5810d74d07b29226adcd80b06d42a7a1446
LuanVitorCD <luanvitorcd@gmail.com>
2026-05-13 18:01:35 -0300

    feat: custom cursor system, remastered cursor images and redesigned options menu

--- Scripts Globais/cursor_manager.gd
+++ Scripts Globais/cursor_manager.gd
@@ -7,19 +7,60 @@ var cursor_disabled = preload("res://Assets/Cursor/cursor_disabled.png")
 var cursor_build = preload("res://Assets/Cursor/cursor_build.png")
 var cursor_upgrade = preload("res://Assets/Cursor/cursor_upgrade.png")
 
-func _ready():
-	# Define o cursor padrão
-	Input.set_custom_mouse_cursor(cursor_normal)
-	
-	# Define o cursor de "mãozinha" (Pointing Hand)
-	Input.set_custom_mouse_cursor(cursor_link, Input.CURSOR_POINTING_HAND)
-	
-	# Define o cursor bloqueado
-	Input.set_custom_mouse_cursor(cursor_disabled, Input.CURSOR_FORBIDDEN)
+## Offset do hotspot do cursor (coordenadas de pixel que é considerado o ponto de clique)
+@export var cursor_offset = Vector2(10, 10)
 
-	# Define o cursor para dar upgrade
-	Input.set_custom_mouse_cursor(cursor_upgrade, Input.CURSOR_DRAG)
+##
+@export var cursor_size: float = 1.5
+
+##
+@export var cursor_color: Color = Color.WHITE
+
+func _ready():
+	update_cursors()
+
+## Aplica as configurações de tamanho e cor aos cursores
+func update_cursors():
+	var scaled_normal = _prepare_cursor(cursor_normal)
+	var scaled_link = _prepare_cursor(cursor_link)
+	var scaled_disabled = _prepare_cursor(cursor_disabled)
+	var scaled_upgrade = _prepare_cursor(cursor_upgrade)
+	var scaled_build = _prepare_cursor(cursor_build)
+	var final_offset = cursor_offset * cursor_size
+
+	Input.set_custom_mouse_cursor(scaled_normal, Input.CURSOR_ARROW, final_offset)
+	Input.set_custom_mouse_cursor(scaled_link, Input.CURSOR_POINTING_HAND, final_offset)
+	Input.set_custom_mouse_cursor(scaled_disabled, Input.CURSOR_FORBIDDEN, final_offset)
+	Input.set_custom_mouse_cursor(scaled_upgrade, Input.CURSOR_DRAG, final_offset)
+	Input.set_custom_mouse_cursor(scaled_build, Input.CURSOR_CROSS, final_offset)
+
+func _prepare_cursor(tex: Texture2D) -> ImageTexture:
+	var img = tex.get_image()
+	if img.is_compressed():
+		img.decompress()
 	
-	# Define o cursor para construir
-	Input.set_custom_mouse_cursor(cursor_build, Input.CURSOR_CROSS)
+	# Recolorir: Substitui pixels brancos pela cor escolhida
+	if cursor_color != Color.WHITE:
+		for y in range(img.get_height()):
+			for x in range(img.get_width()):
+				var pixel = img.get_pixel(x, y)
+				# Verifica se o pixel é branco (ou muito próximo de branco) e não é transparente
+				if pixel.r > 0.5 and pixel.g > 0.5 and pixel.b > 0.5 and pixel.a > 0:
+					img.set_pixel(x, y, cursor_color)
 	
+	var new_width = int(img.get_width() * cursor_size)
+	var new_height = int(img.get_height() * cursor_size)
+	
+	img.resize(new_width, new_height, Image.INTERPOLATE_LANCZOS)
+	
+	return ImageTexture.create_from_image(img)
+
+## Altera o tamanho do cursor e recarrega as texturas
+func set_cursor_scale(scale: float) -> void:
+	cursor_size = scale
+	update_cursors()
+
+## Altera a cor do cursor e recarrega as texturas
+func set_cursor_color(color: Color) -> void:
+	cursor_color = color
+	update_cursors()
--- Scripts Locais/configuracoes.gd
+++ Scripts Locais/configuracoes.gd
@@ -6,16 +6,38 @@ const SETTINGS_PATH = "user://settings.cfg"
 
 var master_bus: int = -1
 
-@onready var _slider_master:  HSlider     = get_node_or_null("CenterContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxMaster/HSlider")
-@onready var _lbl_master:     Label       = get_node_or_null("CenterContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxMaster/LabelPctMaster")
-@onready var _slider_musica:  HSlider     = get_node_or_null("CenterContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxMusica/HSliderMusica")
-@onready var _lbl_musica:     Label       = get_node_or_null("CenterContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxMusica/LabelPctMusica")
-@onready var _check_mudo:     CheckButton = get_node_or_null("CenterContainer/PanelContainer/MarginContainer/VBoxContainer/CheckMudo")
-@onready var _check_tela:     CheckButton = get_node_or_null("CenterContainer/PanelContainer/MarginContainer/VBoxContainer/CheckTelaCheia")
+# Variáveis temporárias para o preview do cursor não afetar o jogo imediatamente
+var temp_cursor_size: float = 1.5
+var temp_cursor_color: Color = Color.WHITE
+
+@onready var _slider_master:  HSlider     = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Esquerda/CardAudio/MarginAudio/VBoxAudio/HBoxMaster/SliderMaster")
+@onready var _lbl_master:     Label       = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Esquerda/CardAudio/MarginAudio/VBoxAudio/HBoxMaster/LabelPctMaster")
+
+@onready var _slider_musica:  HSlider     = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Esquerda/CardAudio/MarginAudio/VBoxAudio/HBoxMusica/SliderMusica")
+@onready var _lbl_musica:     Label       = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Esquerda/CardAudio/MarginAudio/VBoxAudio/HBoxMusica/LabelPctMusica")
+
+@onready var _slider_voz:     HSlider     = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Esquerda/CardAudio/MarginAudio/VBoxAudio/HBoxVoz/SliderVoz")
+@onready var _lbl_voz:        Label       = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Esquerda/CardAudio/MarginAudio/VBoxAudio/HBoxVoz/LabelPctVoz")
+
+@onready var _check_mudo:     CheckButton = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Esquerda/CardVideo/MarginVideo/VBoxVideo/CheckMudo")
+@onready var _check_tela:     CheckButton = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Esquerda/CardVideo/MarginVideo/VBoxVideo/CheckTelaCheia")
+@onready var _check_hud:      CheckButton = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Esquerda/CardVideo/MarginVideo/VBoxVideo/CheckHUD")
+
+@onready var _slider_cursor:  HSlider     = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Direita/CardCursor/MarginCursor/VBoxCursor/HBoxCursor/SliderCursor")
+@onready var _lbl_cursor:     Label       = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Direita/CardCursor/MarginCursor/VBoxCursor/HBoxCursor/LabelScaleCursor")
+@onready var _color_picker:   ColorPickerButton = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Direita/CardCursor/MarginCursor/VBoxCursor/ColorPickerCursor")
+
+@onready var _preview_cursor: TextureRect = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Direita/CardCursor/MarginCursor/VBoxCursor/PreviewArea/PreviewCursor")
+
+@onready var _card_cursor:    Control     = get_node_or_null("CenterContainer/Painel/Margin/VBoxRoot/Conteudo/Direita")
 
 func _ready():
 	master_bus = AudioServer.get_bus_index("Master")
 	_carregar_configuracoes()
+
+	if OS.has_feature("mobile") or OS.has_feature("web_android") or OS.has_feature("web_ios"):
+		if _card_cursor:
+			_card_cursor.hide()
 
 	if _slider_master:
 		_slider_master.value = db_to_linear(AudioServer.get_bus_volume_db(master_bus))
@@ -32,6 +54,21 @@ func _ready():
 		_check_tela.button_pressed = (
 			DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
 		)
+		
+	if has_node("/root/CursorManager"):
+		var manager = get_node("/root/CursorManager")
+		temp_cursor_size = manager.cursor_size
+		temp_cursor_color = manager.cursor_color
+		
+		if _slider_cursor:
+			_slider_cursor.value = temp_cursor_size
+			_lbl_cursor.text = "%.1fx" % temp_cursor_size
+		
+		if _color_picker:
+			_color_picker.color = temp_cursor_color
+			
+		if _preview_cursor:
+			_atualizar_preview_cursor()
 
 # ==========================================
 # HELPERS
@@ -46,6 +83,11 @@ func _musica_linear() -> float:
 		return db_to_linear(mg.volume_db)
 	return 1.0
 
+func _atualizar_preview_cursor() -> void:
+	if _preview_cursor:
+		_preview_cursor.modulate = temp_cursor_color
+		_preview_cursor.custom_minimum_size = Vector2(48 * temp_cursor_size, 48 * temp_cursor_size)
+
 # ==========================================
 # SINAIS DOS CONTROLES
 # ==========================================
@@ -56,25 +98,48 @@ func _on_h_slider_value_changed(value: float) -> void:
 		AudioServer.set_bus_mute(master_bus, false)
 		if _check_mudo:
 			_check_mudo.button_pressed = false
-	_salvar_configuracoes()
 
 func _on_slider_musica_changed(value: float) -> void:
 	var mg = get_node_or_null("/root/MusicaGlobal")
 	if mg:
 		mg.volume_db = linear_to_db(value)
 	_atualizar_pct(_lbl_musica, value)
-	_salvar_configuracoes()
+	
+func _on_slider_voz_changed(value: float) -> void:
+	_atualizar_pct(_lbl_voz, value)
 
 func _on_check_mudo_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(master_bus, toggled_on)
-	_salvar_configuracoes()
 
 func _on_check_tela_cheia_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_check_hud_toggled(_toggled_on: bool) -> void:
+	pass
+
+func _on_cursor_scale_changed(value: float) -> void:
+	temp_cursor_size = value
+	if _lbl_cursor:
+		_lbl_cursor.text = "%.1fx" % value
+	_atualizar_preview_cursor()
+
+func _on_cursor_color_changed(color: Color) -> void:
+	temp_cursor_color = color
+	_atualizar_preview_cursor()
+
+func _on_btn_salvar_pressed() -> void:
+	if has_node("/root/CursorManager"):
+		var manager = get_node("/root/CursorManager")
+		manager.set_cursor_scale(temp_cursor_size)
+		manager.set_cursor_color(temp_cursor_color)
+		
 	_salvar_configuracoes()
+
+func _on_btn_equipe_pressed() -> void:
+	pass
 
 func _on_button_pressed() -> void:
 	fechar_configuracoes.emit()
@@ -86,9 +151,19 @@ func _salvar_configuracoes() -> void:
 	var cfg := ConfigFile.new()
 	cfg.set_value("audio", "master",  db_to_linear(AudioServer.get_bus_volume_db(master_bus)))
 	cfg.set_value("audio", "musica",  _musica_linear())
+	
+	if _slider_voz:
+		cfg.set_value("audio", "voz", _slider_voz.value)
+		
 	cfg.set_value("audio", "mudo",    AudioServer.is_bus_mute(master_bus))
-	cfg.set_value("video", "tela_cheia",
-		DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN)
+	cfg.set_value("video", "tela_cheia", DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN)
+	
+	if _check_hud:
+		cfg.set_value("video", "hud_customizado", _check_hud.button_pressed)
+		
+	cfg.set_value("cursor", "tamanho", temp_cursor_size)
+	cfg.set_value("cursor", "cor", temp_cursor_color)
+		
 	cfg.save(SETTINGS_PATH)
 
 func _carregar_configuracoes() -> void:
@@ -103,6 +178,9 @@ func _carregar_configuracoes() -> void:
 	var mg = get_node_or_null("/root/MusicaGlobal")
 	if mg:
 		mg.volume_db = linear_to_db(vol_musica)
+		
+	if _slider_voz:
+		_slider_voz.value = cfg.get_value("audio", "voz", 1.0)
 
 	var mudo: bool = cfg.get_value("audio", "mudo", false)
 	AudioServer.set_bus_mute(master_bus, mudo)
@@ -110,3 +188,9 @@ func _carregar_configuracoes() -> void:
 	var tela_cheia: bool = cfg.get_value("video", "tela_cheia", false)
 	if tela_cheia:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		
+	if _check_hud:
+		_check_hud.button_pressed = cfg.get_value("video", "hud_customizado", true)
+		
+	temp_cursor_size = cfg.get_value("cursor", "tamanho", 1.5)
+	temp_cursor_color = cfg.get_value("cursor", "cor", Color.WHITE)
